488372a96cf7e842ecb0a5da9e333e3cca5ba9d8
JAQI84 <ja.quinteroi@lanuevaamerica.edu.co>
2025-08-15 18:15:15 -0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Tienda_Planta_Final",
+  base: "/Shop_Plant_Final",
   plugins: [react()],
 });
